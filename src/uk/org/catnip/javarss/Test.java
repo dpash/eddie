00cb0f7d1e3fce2f40b4c2c286fc985c820fdbd6
david <david@350620d1-6314-0410-8468-f513cfc4305e>
2006-05-22 23:23:10 +0000

    * Redesign the java->python conversion * Change the logging formats at each priority * Actually check the results of the python code, not just that it can be run
    git-svn-id: svn+ssh://elmer.catnip.org.uk/home/svn/eddie/trunk@3 350620d1-6314-0410-8468-f513cfc4305e

--- src/uk/org/catnip/javarss/Test.java
+++ src/uk/org/catnip/javarss/Test.java
@@ -7,14 +7,11 @@ import org.apache.log4j.Logger;
 import uk.org.catnip.javarss.parser.Parser;
 import org.python.util.PythonInterpreter;
 import org.python.core.*;
-import java.util.Arrays;
 
 public class Test {
     static Logger log = Logger.getLogger(Test.class);
 
     private PythonInterpreter interp = new PythonInterpreter();
-
-    private int debug = 0;
 
     public int total_tests = 0;
 
@@ -32,14 +29,17 @@ public class Test {
             String[] dir = file_or_dir.list();
             java.util.Arrays.sort(dir);
             for (int i = 0; i < dir.length; i++) {
-                File dir_entry = new File(dirname + "/" + dir[i]);
-                if (dir_entry.isDirectory()) {
-                    System.out.println("descending into " + dir[i]);
-                    parse_dir(dirname + "/" + dir[i]);
-                    System.out.println();
-                } else if (dir_entry.isFile()) {
+                if (!dir[i].equals(".svn")) {
 
-                    test(dirname + "/" + dir[i]);
+                    File dir_entry = new File(dirname + "/" + dir[i]);
+                    if (dir_entry.isDirectory()) {
+                        System.out.println("descending into " + dir[i]);
+                        parse_dir(dirname + "/" + dir[i]);
+                        System.out.println();
+                    } else if (dir_entry.isFile()) {
+
+                        test(dirname + "/" + dir[i]);
+                    }
                 }
             }
         }
@@ -70,19 +70,19 @@ public class Test {
                 }
             }
             in.close();
-            if (log.isInfoEnabled()) {
-                log.info(filename);
-                log.info("Description: " + description);
-                log.info("Test: " + test);
+            if (log.isDebugEnabled()) {
+                log.debug(filename);
+                log.debug("Description: " + description);
+                log.debug("Test: " + test);
             }
             Parser parser = new Parser();
             Feed feed = parser.parse(filename);
-            if (log.isInfoEnabled()) {
-                log.info(feed);
+            if (log.isDebugEnabled()) {
+                log.debug(feed);
             }
             runPython(feed, test);
             if (log.isInfoEnabled()) {
-                log.info("parse passed");
+                log.info("passed: "+ filename);
             } else {
                 System.out.print(".");
             }
@@ -92,7 +92,7 @@ public class Test {
             if (log.isDebugEnabled()) {
                 log.debug("parse failed: " + ex);
             } else if (log.isInfoEnabled()){
-                log.info("parse failed: " + ex.getCause());
+                log.info("failed: " + filename);
             } else {
                 System.out.print("x");
             }
@@ -103,57 +103,136 @@ public class Test {
 
     }
 
-    public void runPython(Feed feed, String test) {
+    public void runPython(Feed feed, String test) throws Exception {
 
         if (feed.error) {
             interp.set("bozo", new PyInteger(1));
         } else {
             interp.set("bozo", new PyInteger(0));
         }
+
+        PyList entries_list = new PyList();
+        Iterator entries = feed.entries();
+        while (entries.hasNext()) {
+            entries_list.append(convertEntry((Entry) entries.next()));
+        }
+
+
+        interp.set("feed", convertFeed(feed));
+        interp.set("entries", entries_list);
+        interp.exec("ret ="+ test);
+        PyInteger ret = (PyInteger)interp.get("ret");
+        if (ret.getValue() == 0) { 
+            throw new Exception("test failed");
+          
+        }
+
+    }
+    public PyDictionary convertFeed(Feed feed) {
         PyDictionary feed_dict = new PyDictionary();
         Iterator it = feed.keys();
         while (it.hasNext()) {
             String key = (String) it.next();
             feed_dict.__setitem__(key, new PyString(feed.get(key)));
         }
-
-        PyList entries_list = new PyList();
-        Iterator entries = feed.entries();
-        while (entries.hasNext()) {
-            PyDictionary entry_dict = new PyDictionary();
-            Entry entry = (Entry) entries.next();
-            log.debug("adding " + entry);
-            Iterator entry_it = entry.keys();
-            while (entry_it.hasNext()) {
-                String key = (String) entry_it.next();
-                entry_dict.__setitem__(key, new PyString(entry.get(key)));
-            }
-            if (entry.getAuthor() != null) {
-                PyDictionary author_detail = new PyDictionary();
-                author_detail.__setitem__("name", new PyString(entry
-                        .getAuthor().name));
-                author_detail.__setitem__("email", new PyString(entry
-                        .getAuthor().email));
-                author_detail.__setitem__("url", new PyString(
-                        entry.getAuthor().url));
-                entry_dict.__setitem__("author_detail", author_detail);
-            }
-            entries_list.append(entry_dict);
-        }
-
         if (feed.getAuthor() != null) {
-            PyDictionary author_detail = new PyDictionary();
-            author_detail.__setitem__("name", new PyString(
-                    feed.getAuthor().name));
-            author_detail.__setitem__("email", new PyString(
-                    feed.getAuthor().email));
-            author_detail
-                    .__setitem__("url", new PyString(feed.getAuthor().url));
-            feed_dict.__setitem__("author_detail", author_detail);
+            feed_dict.__setitem__("author_detail", convertAuthor(feed.getAuthor()));
         }
-        interp.set("feed", feed_dict);
-        interp.set("entries", entries_list);
-        interp.exec(test);
+        if (feed.getTitle() != null) {
+            feed_dict.__setitem__("title_detail",convertDetail(feed.getTitle()));
+        }
+        if (feed.getTagline() != null) {
+            feed_dict.__setitem__("tagline_detail",convertDetail(feed.getTagline()));
+        }
+        if (feed.getGenerator() != null) {
+            feed_dict.__setitem__("generator_detail",convertGenerator(feed.getGenerator()));
+        }
+        PyList contributors_list = new PyList();
+        Iterator contributors = feed.contributors();
+        while (contributors.hasNext()) {
+            contributors_list.append(convertAuthor((Author)contributors.next()));
+        }
+        feed_dict.__setitem__("contributors",contributors_list);
+        
+        
+        PyList links_list = new PyList();
+        Iterator links = feed.links();
+        while (links.hasNext()) {
+            links_list.append(convertLink((Link)links.next()));
+        }
+        feed_dict.__setitem__("links",links_list);
+        return feed_dict;
+        
+    }
+    public PyDictionary convertAuthor(Author author) {
+        PyDictionary author_detail = new PyDictionary();
+        author_detail.__setitem__("name", new PyString(author.getName()));
+        author_detail.__setitem__("email", new PyString(author.getEmail()));
+        author_detail.__setitem__("url", new PyString(author.getUrl()));
+        return author_detail;
+    }
+    
 
+    
+    
+    public PyDictionary convertEntry(Entry entry) {
+        PyDictionary entry_dict = new PyDictionary();
+        Iterator entry_it = entry.keys();
+        while (entry_it.hasNext()) {
+            String key = (String) entry_it.next();
+            entry_dict.__setitem__(key, new PyString(entry.get(key)));
+        }
+        if (entry.getAuthor() != null) {
+            entry_dict.__setitem__("author_detail", convertAuthor(entry.getAuthor()));
+        }
+        if (entry.getTitle() != null) {
+            entry_dict.__setitem__("title_detail",convertDetail(entry.getTitle()));
+        }
+        
+        
+        PyList contents_list = new PyList();
+        Iterator contents = entry.contents();
+        while (contents.hasNext()) {
+            contents_list.append(convertDetail((Detail)contents.next()));
+        }
+        entry_dict.__setitem__("content",contents_list);
+        
+        PyList contributors_list = new PyList();
+        Iterator contributors = entry.contributors();
+        while (contributors.hasNext()) {
+            contributors_list.append(convertAuthor((Author)contributors.next()));
+        }
+        entry_dict.__setitem__("contributors",contributors_list);
+        
+        PyList links_list = new PyList();
+        Iterator links = entry.links();
+        while (links.hasNext()) {
+            links_list.append(convertLink((Link)links.next()));
+        }
+        entry_dict.__setitem__("links",links_list);
+        
+        return entry_dict;
+    }
+    
+    public PyDictionary convertDetail(Detail detail) {
+        PyDictionary detail_dict = new PyDictionary();
+        detail_dict.__setitem__("language", new PyString(detail.getLanguage()));
+        detail_dict.__setitem__("type", new PyString(detail.getType()));
+        detail_dict.__setitem__("value", new PyString(detail.getValue()));
+        return detail_dict;
+    }
+    public PyDictionary convertLink(Link link) {
+        PyDictionary link_dict = convertDetail(link);
+        link_dict.__setitem__("href", new PyString(link.getHref()));
+        link_dict.__setitem__("title", new PyString(link.getTitle()));
+        link_dict.__setitem__("rel", new PyString(link.getRel()));
+        return link_dict;
+    }
+    public PyDictionary convertGenerator(Generator generator) {
+        PyDictionary link_dict = convertDetail(generator);
+        link_dict.__setitem__("name", new PyString(generator.getName()));
+        link_dict.__setitem__("url", new PyString(generator.getUrl()));
+        link_dict.__setitem__("version", new PyString(generator.getVersion()));
+        return link_dict;
     }
 }
