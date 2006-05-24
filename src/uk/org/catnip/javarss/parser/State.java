503a8759f42b0f3b282612293e41d81478c12f53
david <david@350620d1-6314-0410-8468-f513cfc4305e>
2006-05-24 07:10:11 +0000

    Initial namespace support. Most definately broken
    git-svn-id: svn+ssh://elmer.catnip.org.uk/home/svn/eddie/trunk@19 350620d1-6314-0410-8468-f513cfc4305e

--- src/uk/org/catnip/javarss/parser/State.java
+++ src/uk/org/catnip/javarss/parser/State.java
@@ -1,52 +1,119 @@
 package uk.org.catnip.javarss.parser;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.AttributesImpl;
 import java.lang.StringBuilder;
 import java.util.Map;
 import java.util.Hashtable;
 import org.apache.log4j.Logger;
+
 public class State {
-    static Logger log = Logger.getLogger(BaseSAXParser.class);
+    static Logger log = Logger.getLogger(State.class);
+
     private static Map element_aliases = createElementAliases();
-    private static Map createElementAliases(){
+
+    private static Map namespace_aliases = createNamespaceAliases();
+
+private static Map createNamespaceAliases() {
         Map aliases = new Hashtable();
-        aliases.put("abstract"          , "description");
-        aliases.put("body"              , "content");
-        aliases.put("content_encoded"   , "body");
-        aliases.put("created"           , "dcterms_created");
-        aliases.put("dc_author"         , "author");
-        aliases.put("dc_creator"        , "author");
-        aliases.put("dc_date"           , "modified");
-        aliases.put("dc_language"       , "language");
-        aliases.put("dc_publisher"      , "webmaster");
-        aliases.put("dc_rights"         , "copyright");
-        aliases.put("dc_subject"        , "category");
-        aliases.put("dc_title"          , "title");
-        aliases.put("dcterms:modified"  , "modified");
-        aliases.put("item"              , "entry");
-        aliases.put("feedinfo"          , "channel");
-        aliases.put("fullitem"          , "body");
-        aliases.put("homepage"          , "url");
-        aliases.put("keywords"          , "category");
-        aliases.put("issued"            , "dcterms_issued");
-        aliases.put("managingeditor"    , "author");
-        aliases.put("product"           , "item");
-        aliases.put("producturl"        , "link");
-        aliases.put("pubdate"           , "modified");
-        aliases.put("published"         , "dcterms_created");
-        aliases.put("uri"               , "url");
-        aliases.put("xhtml_body"        , "body");
-        aliases.put("updated"           , "modified");
+        //aliases.put( "", "http://backend.userland.com/rss");
+        //aliases.put( "", "http://blogs.law.harvard.edu/tech/rss");
+        //aliases.put( "", "http://purl.org/rss/1.0/");
+        //aliases.put( "", "http://my.netscape.com/rdf/simple/0.9/");
+        //aliases.put( "", "http://example.com/newformat#");
+        //aliases.put( "", "http://example.com/necho");
+        //aliases.put( "", "http://purl.org/echo/");
+        //aliases.put( "", "uri/of/echo/namespace#");
+        //aliases.put( "", "http://purl.org/pie/");
+        //aliases.put( "", "http://purl.org/atom/ns#");
+        //aliases.put( "", "http://purl.org/rss/1.0/modules/rss091#");
+
+        aliases.put( "admin", "http://webns.net/mvcb/");
+        aliases.put( "ag", "http://purl.org/rss/1.0/modules/aggregation/");
+        aliases.put( "annotate", "http://purl.org/rss/1.0/modules/annotate/");
+        aliases.put( "audio", "http://media.tangent.org/rss/1.0/");
+        aliases.put( "blogChannel", "http://backend.userland.com/blogChannelModule");
+        aliases.put( "cc", "http://web.resource.org/cc/");
+        aliases.put( "creativeCommons", "http://backend.userland.com/creativeCommonsRssModule");
+        aliases.put( "co", "http://purl.org/rss/1.0/modules/company");
+        aliases.put( "content", "http://purl.org/rss/1.0/modules/content/");
+        aliases.put( "cp", "http://my.theinfo.org/changed/1.0/rss/");
+        aliases.put( "dc", "http://purl.org/dc/elements/1.1/");
+        aliases.put( "dcterms", "http://purl.org/dc/terms/");
+        aliases.put( "email", "http://purl.org/rss/1.0/modules/email/");
+        aliases.put( "ev", "http://purl.org/rss/1.0/modules/event/");
+        aliases.put( "icbm", "http://postneo.com/icbm/");
+        aliases.put( "image", "http://purl.org/rss/1.0/modules/image/");
+        aliases.put( "foaf", "http://xmlns.com/foaf/0.1/");
+        aliases.put( "fm", "http://freshmeat.net/rss/fm/");
+        aliases.put( "l", "http://purl.org/rss/1.0/modules/link/");
+        aliases.put( "pingback", "http://madskills.com/public/xml/rss/module/pingback/");
+        aliases.put( "prism", "http://prismstandard.org/namespaces/1.2/basic/");
+        aliases.put( "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+        aliases.put( "rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+        aliases.put( "ref", "http://purl.org/rss/1.0/modules/reference/");
+        aliases.put( "reqv", "http://purl.org/rss/1.0/modules/richequiv/");
+        aliases.put( "search", "http://purl.org/rss/1.0/modules/search/");
+        aliases.put( "slash", "http://purl.org/rss/1.0/modules/slash/");
+        aliases.put( "ss", "http://purl.org/rss/1.0/modules/servicestatus/");
+        aliases.put( "str", "http://hacks.benhammersley.com/rss/streaming/");
+        aliases.put( "sub", "http://purl.org/rss/1.0/modules/subscription/");
+        aliases.put( "sy", "http://purl.org/rss/1.0/modules/syndication/");
+        aliases.put( "taxo", "http://purl.org/rss/1.0/modules/taxonomy/");
+        aliases.put( "thr", "http://purl.org/rss/1.0/modules/threading/");
+        aliases.put( "ti", "http://purl.org/rss/1.0/modules/textinput/");
+        aliases.put( "trackback", "http://madskills.com/public/xml/rss/module/trackback/");
+        aliases.put( "wfw", "http://wellformedweb.org/CommentAPI/");
+        aliases.put( "wiki", "http://purl.org/rss/1.0/modules/wiki/");
+        aliases.put( "soap", "http://schemas.xmlsoap.org/soap/envelope/");
+        aliases.put( "xhtml", "http://www.w3.org/1999/xhtml");
+        aliases.put( "xml", "http://www.w3.org/XML/1998/namespace");
+            return aliases;
+}   
+
+private static Map createElementAliases() {
+        Map aliases = new Hashtable();
+        aliases.put("abstract", "description");
+        aliases.put("body", "content");
+        aliases.put("content_encoded", "body");
+        aliases.put("created", "dcterms_created");
+        aliases.put("dc_author", "author");
+        aliases.put("dc_creator", "author");
+        aliases.put("dc_date", "modified");
+        aliases.put("dc_language", "language");
+        aliases.put("dc_publisher", "webmaster");
+        aliases.put("dc_rights", "copyright");
+        aliases.put("dc_subject", "category");
+        aliases.put("dc_title", "title");
+        aliases.put("dcterms:modified", "modified");
+        aliases.put("item", "entry");
+        aliases.put("feedinfo", "channel");
+        aliases.put("fullitem", "body");
+        aliases.put("homepage", "url");
+        aliases.put("keywords", "category");
+        aliases.put("issued", "dcterms_issued");
+        aliases.put("managingeditor", "author");
+        aliases.put("product", "item");
+        aliases.put("producturl", "link");
+        aliases.put("pubdate", "modified");
+        aliases.put("published", "dcterms_created");
+        aliases.put("uri", "url");
+        aliases.put("xhtml_body", "body");
+        aliases.put("updated", "modified");
         return aliases;
     }
-    public State() {}
+
+    public State() {
+    }
+
     public State(String uri, String localName, String qName) {
         this.uri = uri;
         this.element = aliasElement(localName);
         this.qName = qName;
     }
-     public State(String uri, String localName, String qName,
-            Attributes atts, State prev) {
+
+    public State(String uri, String localName, String qName, Attributes atts,
+            State prev) {
         this.uri = uri;
         this.element = aliasElement(localName);
         this.qName = qName;
@@ -58,51 +125,86 @@ public class State {
         if (this.type == null || this.type.equals("")) {
             this.type = "text/plain";
         }
-        this.language = atts.getValue("xml:lang");
+        this.language = this.getAttr("xml:lang");
         if (this.language == null || this.language.equals("")) {
+            log.debug(prev);
+            log.debug("language is null or empty: using previous value:" + prev.language);
             this.language = prev.language;
         }
         if (this.language == null || this.language.equals("")) {
-            this.language = "en";
+            this.language = "";
         }
+        log.debug("language is finally set to "+ this.language);
     }
+
     private String uri = "";
+
     private String localName = "";
+
     private String element = "";
+
     public String qName = "";
+
     public Attributes atts = new AttributesImpl();
+
     public String language = "";
+
     public String base = "";
+
     public String mode = "";
+
     public String type = "";
+
     public boolean expectingText = false;
+
     public StringBuilder text = new StringBuilder();
-    
+
     public void addText(String str) {
         text.append(str);
     }
-public String getAttr(String key) {
-    return this.getAttr(key, "");
-}
-public String getAttr(String key, String default_value) {
-    String ret = atts.getValue("", key);
-    if (ret == null) {
-        ret = default_value;
-    }
-    return ret;
-}
-public String getElement() {
-    return element;
-}
-public void setElement(String element) {
-    this.element = element;
-}
 
-private String aliasElement(String element) {
-    if (element_aliases.containsKey(element)) {
-        log.debug("aliasing "+element + " to "+(String)element_aliases.get(element) );
-        return (String)element_aliases.get(element);
+    public String getAttr(String key) {
+        return this.getAttr(key, "");
     }
-    return element;
-}
+
+    public String getAttr(String key, String default_value) {
+        //String namespace;
+        //if (key.indexOf(':') > 0) {
+        //namespace = key.substring(0,key.indexOf(':'));
+        //key = key.substring(key.indexOf(':')+1, key.length());
+        //} else {
+        //    namespace = "";
+        //}
+        //log.debug("{"+namespace+"}"+key);
+        String ret = atts.getValue(key);
+        if (ret == null) {
+            ret = default_value;
+        }
+        log.debug("getAttr: "+ key + " = '" + ret +"'");
+        return ret;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
+    }
+    private String aliasNamespace(String element) {
+        if (namespace_aliases.containsKey(element)) {
+            log.debug("aliasing namespace " + element + " to "
+                    + (String) namespace_aliases.get(element));
+            return (String) namespace_aliases.get(element);
+        }
+        return element;
+    }
+    private String aliasElement(String element) {
+        if (element_aliases.containsKey(element)) {
+            log.debug("aliasing " + element + " to "
+                    + (String) element_aliases.get(element));
+            return (String) element_aliases.get(element);
+        }
+        return element;
+    }
 }
