41fcb3b84f1c7d899bab27c9cdcfecb1b4b8f5f4
david <david@350620d1-6314-0410-8468-f513cfc4305e>
2006-05-26 21:19:56 +0000

    Category class
    git-svn-id: svn+ssh://elmer.catnip.org.uk/home/svn/eddie/trunk@45 350620d1-6314-0410-8468-f513cfc4305e

---
+++ src/uk/org/catnip/eddie/Category.java
@@ -0,0 +1,25 @@
+package uk.org.catnip.eddie;
+
+public class Category {
+private String term;
+private String schedule;
+private String label;
+public String getLabel() {
+    return label;
+}
+public void setLabel(String label) {
+    this.label = label;
+}
+public String getSchedule() {
+    return schedule;
+}
+public void setSchedule(String schedule) {
+    this.schedule = schedule;
+}
+public String getTerm() {
+    return term;
+}
+public void setTerm(String term) {
+    this.term = term;
+}
+}
