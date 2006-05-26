ec7209f8279e6f5a3a16dcc2f705703497df521e
david <david@350620d1-6314-0410-8468-f513cfc4305e>
2006-05-26 23:38:42 +0000

    add image class
    git-svn-id: svn+ssh://elmer.catnip.org.uk/home/svn/eddie/trunk@51 350620d1-6314-0410-8468-f513cfc4305e

---
+++ src/uk/org/catnip/eddie/Image.java
@@ -0,0 +1,46 @@
+package uk.org.catnip.eddie;
+
+public class Image {
+    String title;
+    String href;
+    String link;
+    String width;
+    String height;
+    String description;
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getHeight() {
+        return height;
+    }
+    public void setHeight(String height) {
+        this.height = height;
+    }
+    public String getHref() {
+        return href;
+    }
+    public void setHref(String href) {
+        this.href = href;
+    }
+    public String getLink() {
+        return link;
+    }
+    public void setLink(String link) {
+        this.link = link;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getWidth() {
+        return width;
+    }
+    public void setWidth(String width) {
+        this.width = width;
+    }
+}
