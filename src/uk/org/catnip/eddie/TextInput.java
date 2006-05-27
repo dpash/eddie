6221562fab439e343714dcb73ef0a362d4d79626
david <david@350620d1-6314-0410-8468-f513cfc4305e>
2006-05-27 09:52:14 +0000

    Flesh out TextInput class
    git-svn-id: svn+ssh://elmer.catnip.org.uk/home/svn/eddie/trunk@60 350620d1-6314-0410-8468-f513cfc4305e

--- src/uk/org/catnip/eddie/TextInput.java
+++ src/uk/org/catnip/eddie/TextInput.java
@@ -1,5 +1,43 @@
 package uk.org.catnip.eddie;
 
-public class TextInput extends FeedContext {
-
+public class TextInput {
+    String title;
+    String link;
+    String name;
+    String description;
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getLink() {
+        return link;
+    }
+    public void setLink(String link) {
+        this.link = link;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        sb.append("title = '" + title + "', ");
+        sb.append("link = '" + link + "', ");
+        sb.append("name = '" + name + "', ");
+        sb.append("description = '" + description + "'");
+        sb.append("}");
+        return sb.toString();
+    }
 }
