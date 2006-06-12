95d27b62746c367820f55dc38cdc09dd9c4b6530
david <david@350620d1-6314-0410-8468-f513cfc4305e>
2006-06-12 01:57:51 +0000

    Simplify the Feed class. It doesn't need to know about timestamp.
    git-svn-id: svn+ssh://elmer.catnip.org.uk/home/svn/eddie/trunk@201 350620d1-6314-0410-8468-f513cfc4305e

--- src/uk/org/catnip/eddie/spider/Feed.java
+++ src/uk/org/catnip/eddie/spider/Feed.java
@@ -1,16 +1,17 @@
 package uk.org.catnip.eddie.spider;
 
-import java.util.*;
-import java.sql.*;
+import java.util.Date;
+import java.util.List;
 import org.apache.log4j.Logger;
+
 public class Feed extends SQLObject {
 	private static Logger log = Logger.getLogger(Feed.class);
 
 	SQLObjectDesc description = new FeedDesc();
 	private String feedUrl;
-	private java.util.Date lastModified;
-	private java.util.Date lastChecked;
-	private java.util.Date nextCheck;
+	private Date lastModified;
+	private Date lastChecked;
+	private Date nextCheck;
 	public Feed() {}
 	public Feed(int id) {
 		// TODO: fill this feed in from the database
@@ -22,62 +23,36 @@ public class Feed extends SQLObject {
 		this.feedUrl = feedUrl;
 	}
 
-	public java.util.Date getLast_checked() {
+	public Date getLast_checked() {
 		return lastChecked;
 	}
-	public void setLastChecked(java.util.Date lastChecked) {
-		if (lastChecked != null) {
-		 this.lastChecked = new java.sql.Timestamp(lastChecked.getTime());
-		}
+	public void setLastChecked(Date lastChecked) {
+		this.lastChecked = lastChecked;
 	}
 	
 	public List<SQLObject> getEntries() {
 		return Entry.retrieveWhere("feed = ?", getId());
 	}
 	
-	public String updateSQL() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("UPDATE ");
-		sb.append(description.tablename());
-		sb.append(" SET ");
-		sb.append("feed_url='");
-		sb.append(feedUrl);
-		sb.append("', last_checked='");
-		sb.append(lastChecked);
-		sb.append("', next_check='");
-		sb.append(nextCheck);
-		if (lastModified != null) { 
-		sb.append("', last_modified='");
-		sb.append(lastModified);
-		}
-		sb.append("' WHERE ");
-		sb.append(description.id_column());
-		sb.append(" = ");
-		sb.append(getId());
-		sb.append(";");
-		log.info(sb);
-		return sb.toString();
-	}
-	
-
-	public static SQLObject getNewObject() { return new Feed(); }
-
 	public void update() { super.update(description); }
 
-	public java.util.Date getNext_check() {
+	public Date getNext_check() {
 		return nextCheck;
 	}
-	public void setNextCheck(java.util.Date nextCheck) {
-		this.nextCheck = new java.sql.Timestamp(nextCheck.getTime());
+	
+	public void setNextCheck(Date nextCheck) {
+		this.nextCheck = nextCheck;
 	}
+	
 	public void scheduleNextCheck() {
-		java.util.Date now = new java.util.Date();
-		setNextCheck(new java.sql.Timestamp(now.getTime() + 3600*1000));
+		Date now = new Date();
+		now.setTime(now.getTime() + 3600000); // Check again in an hour
+		setNextCheck(now);
 	}
-	public java.util.Date getLast_modified() {
+	public Date getLast_modified() {
 		return lastModified;
 	}
-	public void setLastModified(java.util.Date lastModified) {
+	public void setLastModified(Date lastModified) {
 		this.lastModified = lastModified;
 	}
 	static List<SQLObject> retrieveWhere(String where_clause, Object...parameters) {
