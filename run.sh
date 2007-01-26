d43b061dbb583f0d089750f81e75b99e568edb7b
david <david@350620d1-6314-0410-8468-f513cfc4305e>
2007-01-26 19:15:20 +0000

    fix the test run script
    git-svn-id: svn+ssh://elmer.catnip.org.uk/home/svn/eddie/trunk@257 350620d1-6314-0410-8468-f513cfc4305e

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export CLASSPATH="/usr/share/java/xercesImpl.jar:/usr/share/java/log4j-1.2.jar:lib/jython.jar:eddie.jar:eddie-test.jar"
-export JAVA_HOME=/usr/lib/jvm/java-1.5.0-sun
+export CLASSPATH="lib/tagsoup-1.0.1.jar:/usr/share/java/commons-codec.jar:/usr/share/java/xercesImpl.jar:/usr/share/java/log4j-1.2.jar:lib/jython.jar:eddie.jar:eddie-test.jar"
+export JAVA_HOME=/usr/lib/jvm/java-6-sun
 $JAVA_HOME/bin/java uk.org.catnip.eddie.parser.Main "$@"
 
